f732f52abe8561144b61c76d6db51fb47e9032be
Arya Hemin <ariahemin@gmail.com>
2023-06-08 17:42:18 +0300

    so i just need to be focused on the window which is duh 1000 times better the performance is not bad either so it works? happy

--- src/gimmicks/the moving circle era/simple moving circle js/simpleMovingCIrcleJs.jsx
+++ src/gimmicks/the moving circle era/simple moving circle js/simpleMovingCIrcleJs.jsx
@@ -1,22 +1,32 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 
 export default function SimpleMovingCircleJs(){
+    // const ref = useRef(null);
     const [position, setPosition] = useState(0)
 
-    function move (x){
-        x === "right" ? setPosition(position + 10) : x === "left" ? setPosition(position - 10) : console.log("nothing")
-    }
+    // inputElement.addEventListener("keydown", handleKeyDown);
+    
+    useEffect(() => {
+        function move (x){
+            x === "right" ? setPosition(position + 10) : x === "left" ? setPosition(position - 10) : console.log("nothing")
+        }
 
+        const handleKeyDown = e => {
+            e.key === "ArrowRight" ? move("right") : e.key === "ArrowLeft" ? move("left") : null
+        };
+    
+        window.addEventListener('keydown', handleKeyDown);
+    
+        return () => {
+            console.log(position)
+            window.removeEventListener('keydown', handleKeyDown);
+        };
 
-    const handleKeyDown = e => {
-        console.log(e.key)
-        e.key === "ArrowRight" ? move("right") : e.key === "ArrowLeft" ? move("left") : null
-    };
-      
+      });
 
     return(
-        <div tabIndex={-1} onKeyDown={handleKeyDown}>
+        <div>
             <div>i am a moving circle that move on key press</div>     
             <div className="w-[500px] h-[500px] bg-gradient-to-r from-purple-500 to-pink-500 rounded " >
                 <div  className="relative m-0 w-[50px] h-[50px] bg-white rounded-full" style={{ transform: `translate(${position}px)`}} ></div>
